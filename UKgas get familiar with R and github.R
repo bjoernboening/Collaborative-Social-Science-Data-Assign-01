26b030af9d519bcc953c1fa6304a58d6cee8fca2
bjoernboening <bjoboe@t-online.de>
2015-10-02 01:36:17 +0200

    First files
    Here is the first file upload from the project, including a R file and a
    screenshot of a graph which answers question #1 from my readme.

---
+++ UKgas get familiar with R and github.R
@@ -0,0 +1,24 @@
+#########################################################
+# Assignment 01 Collaborative Social Sciene Data Analysis
+# Björn Boening | bjoernboening
+# UKgas dataset from R
+# 30 Sept 2015
+
+install.packages("rmmarkdown") # to hopefully make cool presentations in the future :D
+data(UKgas) # load dataset
+?UKgas # data signature
+hist(UKgas) # view a histogram
+ls(UKgas) #list objects - okay, there are no objects
+names(UKgas) #list variables - yep, no variables, but we could generate some
+str(UKgas) # data structure - no useful data structure so far to answer both research questions 
+levels(UKgas) # data levels - nope! no data levels
+ls(UKgas) # list UKgas again
+summary(UKgas) # basic summary statistics
+sort(UKgas) # here I want to sort my data and reshape it, *i miss excel*
+length(UKgas)
+test<-UKgas[sequence(1,108,2)]
+test <- UKgas[seq]
+boxplot(UKgas)
+plot(UKgas) # well, it didnt work but at least I can show a plot which shows the answer to question #1 in my readme.
+
+### next assignment follows
